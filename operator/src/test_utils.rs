d22edd59afb835a3bb53e46b21227a29ceae5c32
Beñat Gartzia Arruabarrena <bgartzia@redhat.com>
2025-12-22 16:41:22 +0100

    trustee.tests: Make dummy_pcrs a bit less dummy
    trustee tests were relying on some values that didn't hold any TPMEvent
    or any PCR value that related to them.
    
    This commit turns those dummy values into something that are closer to
    something we could expect in reality.
    
    The main reason to do this is that the compute-pcrs logic does not like
    empty event PCRs, as it's based on that to reconstruct them.
    
    This goes against the purpose of a unit test, as we should actually mock
    that external part so as not to rely on it, but decided this was simpler
    at least as an initial proposal.
    
    Signed-off-by: Beñat Gartzia Arruabarrena <bgartzia@redhat.com>

--- operator/src/test_utils.rs
+++ operator/src/test_utils.rs
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: MIT
 
 use compute_pcrs_lib::Pcr;
+use compute_pcrs_lib::tpmevents::{TPMEvent, TPMEventID};
 use k8s_openapi::{api::core::v1::ConfigMap, jiff::Timestamp};
 use kube::Client;
 use operator::RvContextData;
@@ -11,6 +12,11 @@ use std::collections::BTreeMap;
 use crate::trustee;
 use trusted_cluster_operator_lib::reference_values::{ImagePcr, ImagePcrs, PCR_CONFIG_FILE};
 
+pub const DUMMY_PCR_4_VALUE: &str =
+    "3f263b96ccbc33bb53d808771f9ab1e02d4dec8854f9530f749cde853a723273";
+pub const DUMMY_PCR_7_VALUE: &str =
+    "e58ada1ba75f2e4722b539824598ad5e10c55f2e4aeab2033f3b0a8ee3f3eca6";
+
 pub fn dummy_pcrs() -> ImagePcrs {
     ImagePcrs(BTreeMap::from([(
         "cos".to_string(),
@@ -18,14 +24,30 @@ pub fn dummy_pcrs() -> ImagePcrs {
             first_seen: Timestamp::now(),
             pcrs: vec![
                 Pcr {
-                    id: 0,
-                    value: "pcr0_val".into(),
-                    events: vec![],
+                    id: 4,
+                    value: hex::decode(DUMMY_PCR_4_VALUE).unwrap(),
+                    events: vec![TPMEvent {
+                        name: "EV_EFI_ACTION".into(),
+                        pcr: 4,
+                        hash: hex::decode(
+                            "3d6772b4f84ed47595d72a2c4c5ffd15f5bb72c7507fe26f2aaee2c69d5633ba",
+                        )
+                        .unwrap(),
+                        id: TPMEventID::Pcr4EfiCall,
+                    }],
                 },
                 Pcr {
-                    id: 1,
-                    value: "pcr1_val".into(),
-                    events: vec![],
+                    id: 7,
+                    value: hex::decode(DUMMY_PCR_7_VALUE).unwrap(),
+                    events: vec![TPMEvent {
+                        name: "EV_EFI_VARIABLE_DRIVER_CONFIG".into(),
+                        pcr: 7,
+                        hash: hex::decode(
+                            "ccfc4bb32888a345bc8aeadaba552b627d99348c767681ab3141f5b01e40a40e",
+                        )
+                        .unwrap(),
+                        id: TPMEventID::Pcr7SecureBoot,
+                    }],
                 },
             ],
             reference: "".to_string(),
